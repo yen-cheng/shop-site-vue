9969d146535f9914afcc66ad27aaaa16e4ebb389
YEN-CHENG LIU <c794613852852@gmail.com>
2020-07-06 02:39:26 +0900

    delete dist

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./registerServiceWorker";
 
 import "bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
-window.$ = window.jQuery = require('jquery');
-import axios from "axios"
+import "bootstrap/dist/css/bootstrap.min.css";
+window.$ = window.jQuery = require("jquery");
+import axios from "axios";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-axios.defaults.baseURL = "http://localhost:3000"
+axios.defaults.baseURL = "http://localhost:3000";
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
